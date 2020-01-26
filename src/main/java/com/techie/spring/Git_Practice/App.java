f9806f9d5f811a8be38b2a6df3c26df4f04a8025
mamtaandhe <40206843+mamtaandhe@users.noreply.github.com>
2020-01-26 18:33:00 +0530

    added new method for pull

--- src/main/java/com/techie/spring/Git_Practice/App.java
+++ src/main/java/com/techie/spring/Git_Practice/App.java
@@ -12,6 +12,9 @@ public class App
 			System.out.println("welcome");
 		}
 	}
+	public void addedNewMethodtoTakeUpadte(){
+	System.out.println("added from hithub directly for the purpose of update/pull from our new joinee system");
+	}	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
