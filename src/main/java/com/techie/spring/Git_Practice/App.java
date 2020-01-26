6a77d5d535ad609c3f6506f885678891697a8040
mamta <mamtaandhe14@gmail.com>
2020-01-26 15:49:07 +0530

    added demo project to local repo

---
+++ src/main/java/com/techie/spring/Git_Practice/App.java
@@ -0,0 +1,17 @@
+package com.techie.spring.Git_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data) {
+		System.out.println(data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
